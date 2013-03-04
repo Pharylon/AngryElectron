8bb1744c05d78f9d2f75e73c1344c9093a692b1c
hellsbells <walrusman2@gmail.com>
2013-03-03 19:44:27 -0500

    made Element
    BAMF

---
+++ AngryElectron/Controllers/ElementController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AngryElectronDomain;
+
+namespace AngryElectron.Controllers
+{
+    public class ElementController : Controller
+    {
+        //
+        // GET: /Element/
+
+        public ActionResult Index()
+        {
+            Element hydrogen = new Element("H");
+            return View(hydrogen);
+        }
+
+    }
+}
---
+++ AngryElectronDomain/Element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngryElectronDomain
+{
+    public class Element
+    {
+        public string Symbol { get; private set; }
+
+        public Element(string symbol)
+        {
+            Symbol = symbol;
+        }
+    }
+}
