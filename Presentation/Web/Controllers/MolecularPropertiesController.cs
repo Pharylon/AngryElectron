e2cc4a362f672229fb7fca6fd054c618093cf2c2
Patrick Graham <pabreetzio@gmail.com>
2013-04-01 21:53:13 -0400

    Adding new Views and their respective Controllers.

---
+++ Presentation/Web/Controllers/MolecularPropertiesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngryElectron.Presentation.Web.Controllers
+{
+    public class MolecularPropertiesController : Controller
+    {
+        //
+        // GET: /MolecularProperties/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Presentation/Web/Controllers/ResourcesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngryElectron.Presentation.Web.Controllers
+{
+    public class ResourcesController : Controller
+    {
+        //
+        // GET: /Resources/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
