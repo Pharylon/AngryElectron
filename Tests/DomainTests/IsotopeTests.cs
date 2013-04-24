cd5a77b7141ef0d0c6b88c003a08d7660e40a81e
hellsbells <walrusman2@gmail.com>
2013-04-24 19:40:54 -0400

    Added ParserTest and IsotopeTest Classes

---
+++ Tests/DomainTests/IsotopeTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AngryElectron.Tests.Domain
+{
+    [TestClass]
+    public class IsotopeTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tests/DomainTests/ParserTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngryElectron.Domain;
+
+namespace AngryElectron.Tests.Domain
+{
+    [TestClass]
+    public class ParserTests
+    {
+        Parser SideParser = new Parser();
+        ChemicalEquation SideEquation;
+
+
+
+        //[TestMethod]
+        //public void EquationCanParseOneSideTest()
+        //{
+        //    SideEquation = SideParser.Parse("H2O");
+        //    Assert.IsTrue(SideEquation.ToHTML() == "H<sub>2</sub>O");
+        //}
+    }
+}
