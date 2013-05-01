32a52b7b7a02e3fdd37673c433c4f5b6ddbb3533
hellsbells <walrusman2@gmail.com>
2013-04-30 20:54:14 -0400

    Made Isotope class and tested constructor

--- Tests/DomainTests/IsotopeTests.cs
+++ Tests/DomainTests/IsotopeTests.cs
@@ -1,14 +1,28 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AngryElectron.Domain;
+using System.Linq;
 
 namespace AngryElectron.Tests.Domain
 {
     [TestClass]
     public class IsotopeTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        TableOfElements MyElements;
+
+        [TestInitialize]
+        public void initialize()
         {
+            MyElements = new TableOfElements();
         }
+        [TestMethod]
+        public void IsotopeReturnsAtomicMassItWasCreatedWith()
+        {
+            Isotope MyIsotope = new Isotope(MyElements.Where(E => E.Name == "Hydrogen").FirstOrDefault(), 12);
+            Assert.IsTrue(MyIsotope.AtomicMass == 12);
+            Assert.AreEqual(12, MyIsotope.AtomicMass);
+        }
+
+  
     }
 }
